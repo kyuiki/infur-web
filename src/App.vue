f9b22cf8bb55e3be0f379e7086b635f30c3ee39f
kyuiki <qkie0915@gmail.com>
2024-09-16 19:44:53 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import MapEmbed from "./components/MapEmbedComp.vue";
+</script>
+
+<template>
+  <div class="bg-indigo-950">
+    <MapEmbed />
+  </div>
+</template>
+
+<style scoped>
+  .handwrite {
+    font-family: "Lato";
+  }
+</style>
---
+++ src/components/MapEmbedComp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-2 h-screen flex flex-col">
+    <iframe
+      class="w-full h-full rounded-lg"
+      style="border: 0"
+      loading="lazy"
+      src="https://gayfemboyiffymap.minecra.fr/"
+    ></iframe>
+    <div class="text-white mt-1">
+      <p class="float-right">
+        <span class="text-sm"> Made with ❤️ by </span>
+        <a class="text-blue-300 font-extrabold hover:text-blue-400" href="https://kyuiki.com"> Kyuiki </a>
+      </p>
+      <p>
+        <span class="font-extrabold"> mc.kyuiki.com </span>
+        <span class="text-sm"> Minecraft Server :3 </span>
+      </p>
+    </div>
+  </div>
+</template>
